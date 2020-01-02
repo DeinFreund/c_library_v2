6e4dab158b37a5af0e44bbd45c5de6b04513969e
DeinFreund <flyck@ethz.ch>
2020-01-02 13:21:34 +0100

    add target position to dynamixel_status

--- omav/mavlink_msg_dynamixel_status.h
+++ omav/mavlink_msg_dynamixel_status.h
@@ -6,37 +6,41 @@
 MAVPACKED(
 typedef struct __mavlink_dynamixel_status_t {
  uint64_t time_boot_us; /*< [us] Timestamp (time since system boot).*/
- float angles[8]; /*< [rad] Current angles.*/
+ float anglesMeasured[8]; /*< [rad] Current angle measurements.*/
+ float anglesSet[8]; /*< [rad] Current angle targets.*/
  uint8_t noutputs; /*< [1] Number of measured dynamixels.*/
 }) mavlink_dynamixel_status_t;
 
-#define MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN 41
-#define MAVLINK_MSG_ID_DYNAMIXEL_STATUS_MIN_LEN 41
-#define MAVLINK_MSG_ID_99_LEN 41
-#define MAVLINK_MSG_ID_99_MIN_LEN 41
+#define MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN 73
+#define MAVLINK_MSG_ID_DYNAMIXEL_STATUS_MIN_LEN 73
+#define MAVLINK_MSG_ID_99_LEN 73
+#define MAVLINK_MSG_ID_99_MIN_LEN 73
 
-#define MAVLINK_MSG_ID_DYNAMIXEL_STATUS_CRC 48
-#define MAVLINK_MSG_ID_99_CRC 48
+#define MAVLINK_MSG_ID_DYNAMIXEL_STATUS_CRC 85
+#define MAVLINK_MSG_ID_99_CRC 85
 
-#define MAVLINK_MSG_DYNAMIXEL_STATUS_FIELD_ANGLES_LEN 8
+#define MAVLINK_MSG_DYNAMIXEL_STATUS_FIELD_ANGLESMEASURED_LEN 8
+#define MAVLINK_MSG_DYNAMIXEL_STATUS_FIELD_ANGLESSET_LEN 8
 
 #if MAVLINK_COMMAND_24BIT
 #define MAVLINK_MESSAGE_INFO_DYNAMIXEL_STATUS { \
     99, \
     "DYNAMIXEL_STATUS", \
-    3, \
+    4, \
     {  { "time_boot_us", NULL, MAVLINK_TYPE_UINT64_T, 0, 0, offsetof(mavlink_dynamixel_status_t, time_boot_us) }, \
-         { "noutputs", NULL, MAVLINK_TYPE_UINT8_T, 0, 40, offsetof(mavlink_dynamixel_status_t, noutputs) }, \
-         { "angles", NULL, MAVLINK_TYPE_FLOAT, 8, 8, offsetof(mavlink_dynamixel_status_t, angles) }, \
+         { "noutputs", NULL, MAVLINK_TYPE_UINT8_T, 0, 72, offsetof(mavlink_dynamixel_status_t, noutputs) }, \
+         { "anglesMeasured", NULL, MAVLINK_TYPE_FLOAT, 8, 8, offsetof(mavlink_dynamixel_status_t, anglesMeasured) }, \
+         { "anglesSet", NULL, MAVLINK_TYPE_FLOAT, 8, 40, offsetof(mavlink_dynamixel_status_t, anglesSet) }, \
          } \
 }
 #else
 #define MAVLINK_MESSAGE_INFO_DYNAMIXEL_STATUS { \
     "DYNAMIXEL_STATUS", \
-    3, \
+    4, \
     {  { "time_boot_us", NULL, MAVLINK_TYPE_UINT64_T, 0, 0, offsetof(mavlink_dynamixel_status_t, time_boot_us) }, \
-         { "noutputs", NULL, MAVLINK_TYPE_UINT8_T, 0, 40, offsetof(mavlink_dynamixel_status_t, noutputs) }, \
-         { "angles", NULL, MAVLINK_TYPE_FLOAT, 8, 8, offsetof(mavlink_dynamixel_status_t, angles) }, \
+         { "noutputs", NULL, MAVLINK_TYPE_UINT8_T, 0, 72, offsetof(mavlink_dynamixel_status_t, noutputs) }, \
+         { "anglesMeasured", NULL, MAVLINK_TYPE_FLOAT, 8, 8, offsetof(mavlink_dynamixel_status_t, anglesMeasured) }, \
+         { "anglesSet", NULL, MAVLINK_TYPE_FLOAT, 8, 40, offsetof(mavlink_dynamixel_status_t, anglesSet) }, \
          } \
 }
 #endif
@@ -49,23 +53,26 @@ typedef struct __mavlink_dynamixel_status_t {
  *
  * @param time_boot_us [us] Timestamp (time since system boot).
  * @param noutputs [1] Number of measured dynamixels.
- * @param angles [rad] Current angles.
+ * @param anglesMeasured [rad] Current angle measurements.
+ * @param anglesSet [rad] Current angle targets.
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_dynamixel_status_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
-                               uint64_t time_boot_us, uint8_t noutputs, const float *angles)
+                               uint64_t time_boot_us, uint8_t noutputs, const float *anglesMeasured, const float *anglesSet)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN];
     _mav_put_uint64_t(buf, 0, time_boot_us);
-    _mav_put_uint8_t(buf, 40, noutputs);
-    _mav_put_float_array(buf, 8, angles, 8);
+    _mav_put_uint8_t(buf, 72, noutputs);
+    _mav_put_float_array(buf, 8, anglesMeasured, 8);
+    _mav_put_float_array(buf, 40, anglesSet, 8);
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN);
 #else
     mavlink_dynamixel_status_t packet;
     packet.time_boot_us = time_boot_us;
     packet.noutputs = noutputs;
-    mav_array_memcpy(packet.angles, angles, sizeof(float)*8);
+    mav_array_memcpy(packet.anglesMeasured, anglesMeasured, sizeof(float)*8);
+    mav_array_memcpy(packet.anglesSet, anglesSet, sizeof(float)*8);
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN);
 #endif
 
@@ -81,24 +88,27 @@ static inline uint16_t mavlink_msg_dynamixel_status_pack(uint8_t system_id, uint
  * @param msg The MAVLink message to compress the data into
  * @param time_boot_us [us] Timestamp (time since system boot).
  * @param noutputs [1] Number of measured dynamixels.
- * @param angles [rad] Current angles.
+ * @param anglesMeasured [rad] Current angle measurements.
+ * @param anglesSet [rad] Current angle targets.
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_dynamixel_status_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
                                mavlink_message_t* msg,
-                                   uint64_t time_boot_us,uint8_t noutputs,const float *angles)
+                                   uint64_t time_boot_us,uint8_t noutputs,const float *anglesMeasured,const float *anglesSet)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN];
     _mav_put_uint64_t(buf, 0, time_boot_us);
-    _mav_put_uint8_t(buf, 40, noutputs);
-    _mav_put_float_array(buf, 8, angles, 8);
+    _mav_put_uint8_t(buf, 72, noutputs);
+    _mav_put_float_array(buf, 8, anglesMeasured, 8);
+    _mav_put_float_array(buf, 40, anglesSet, 8);
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN);
 #else
     mavlink_dynamixel_status_t packet;
     packet.time_boot_us = time_boot_us;
     packet.noutputs = noutputs;
-    mav_array_memcpy(packet.angles, angles, sizeof(float)*8);
+    mav_array_memcpy(packet.anglesMeasured, anglesMeasured, sizeof(float)*8);
+    mav_array_memcpy(packet.anglesSet, anglesSet, sizeof(float)*8);
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN);
 #endif
 
@@ -116,7 +126,7 @@ static inline uint16_t mavlink_msg_dynamixel_status_pack_chan(uint8_t system_id,
  */
 static inline uint16_t mavlink_msg_dynamixel_status_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_dynamixel_status_t* dynamixel_status)
 {
-    return mavlink_msg_dynamixel_status_pack(system_id, component_id, msg, dynamixel_status->time_boot_us, dynamixel_status->noutputs, dynamixel_status->angles);
+    return mavlink_msg_dynamixel_status_pack(system_id, component_id, msg, dynamixel_status->time_boot_us, dynamixel_status->noutputs, dynamixel_status->anglesMeasured, dynamixel_status->anglesSet);
 }
 
 /**
@@ -130,7 +140,7 @@ static inline uint16_t mavlink_msg_dynamixel_status_encode(uint8_t system_id, ui
  */
 static inline uint16_t mavlink_msg_dynamixel_status_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_dynamixel_status_t* dynamixel_status)
 {
-    return mavlink_msg_dynamixel_status_pack_chan(system_id, component_id, chan, msg, dynamixel_status->time_boot_us, dynamixel_status->noutputs, dynamixel_status->angles);
+    return mavlink_msg_dynamixel_status_pack_chan(system_id, component_id, chan, msg, dynamixel_status->time_boot_us, dynamixel_status->noutputs, dynamixel_status->anglesMeasured, dynamixel_status->anglesSet);
 }
 
 /**
@@ -139,23 +149,26 @@ static inline uint16_t mavlink_msg_dynamixel_status_encode_chan(uint8_t system_i
  *
  * @param time_boot_us [us] Timestamp (time since system boot).
  * @param noutputs [1] Number of measured dynamixels.
- * @param angles [rad] Current angles.
+ * @param anglesMeasured [rad] Current angle measurements.
+ * @param anglesSet [rad] Current angle targets.
  */
 #ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
 
-static inline void mavlink_msg_dynamixel_status_send(mavlink_channel_t chan, uint64_t time_boot_us, uint8_t noutputs, const float *angles)
+static inline void mavlink_msg_dynamixel_status_send(mavlink_channel_t chan, uint64_t time_boot_us, uint8_t noutputs, const float *anglesMeasured, const float *anglesSet)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN];
     _mav_put_uint64_t(buf, 0, time_boot_us);
-    _mav_put_uint8_t(buf, 40, noutputs);
-    _mav_put_float_array(buf, 8, angles, 8);
+    _mav_put_uint8_t(buf, 72, noutputs);
+    _mav_put_float_array(buf, 8, anglesMeasured, 8);
+    _mav_put_float_array(buf, 40, anglesSet, 8);
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_DYNAMIXEL_STATUS, buf, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_MIN_LEN, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_CRC);
 #else
     mavlink_dynamixel_status_t packet;
     packet.time_boot_us = time_boot_us;
     packet.noutputs = noutputs;
-    mav_array_memcpy(packet.angles, angles, sizeof(float)*8);
+    mav_array_memcpy(packet.anglesMeasured, anglesMeasured, sizeof(float)*8);
+    mav_array_memcpy(packet.anglesSet, anglesSet, sizeof(float)*8);
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_DYNAMIXEL_STATUS, (const char *)&packet, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_MIN_LEN, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_CRC);
 #endif
 }
@@ -168,7 +181,7 @@ static inline void mavlink_msg_dynamixel_status_send(mavlink_channel_t chan, uin
 static inline void mavlink_msg_dynamixel_status_send_struct(mavlink_channel_t chan, const mavlink_dynamixel_status_t* dynamixel_status)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-    mavlink_msg_dynamixel_status_send(chan, dynamixel_status->time_boot_us, dynamixel_status->noutputs, dynamixel_status->angles);
+    mavlink_msg_dynamixel_status_send(chan, dynamixel_status->time_boot_us, dynamixel_status->noutputs, dynamixel_status->anglesMeasured, dynamixel_status->anglesSet);
 #else
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_DYNAMIXEL_STATUS, (const char *)dynamixel_status, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_MIN_LEN, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_CRC);
 #endif
@@ -182,19 +195,21 @@ static inline void mavlink_msg_dynamixel_status_send_struct(mavlink_channel_t ch
   is usually the receive buffer for the channel, and allows a reply to an
   incoming message with minimum stack space usage.
  */
-static inline void mavlink_msg_dynamixel_status_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint64_t time_boot_us, uint8_t noutputs, const float *angles)
+static inline void mavlink_msg_dynamixel_status_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint64_t time_boot_us, uint8_t noutputs, const float *anglesMeasured, const float *anglesSet)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char *buf = (char *)msgbuf;
     _mav_put_uint64_t(buf, 0, time_boot_us);
-    _mav_put_uint8_t(buf, 40, noutputs);
-    _mav_put_float_array(buf, 8, angles, 8);
+    _mav_put_uint8_t(buf, 72, noutputs);
+    _mav_put_float_array(buf, 8, anglesMeasured, 8);
+    _mav_put_float_array(buf, 40, anglesSet, 8);
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_DYNAMIXEL_STATUS, buf, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_MIN_LEN, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_CRC);
 #else
     mavlink_dynamixel_status_t *packet = (mavlink_dynamixel_status_t *)msgbuf;
     packet->time_boot_us = time_boot_us;
     packet->noutputs = noutputs;
-    mav_array_memcpy(packet->angles, angles, sizeof(float)*8);
+    mav_array_memcpy(packet->anglesMeasured, anglesMeasured, sizeof(float)*8);
+    mav_array_memcpy(packet->anglesSet, anglesSet, sizeof(float)*8);
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_DYNAMIXEL_STATUS, (const char *)packet, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_MIN_LEN, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN, MAVLINK_MSG_ID_DYNAMIXEL_STATUS_CRC);
 #endif
 }
@@ -222,17 +237,27 @@ static inline uint64_t mavlink_msg_dynamixel_status_get_time_boot_us(const mavli
  */
 static inline uint8_t mavlink_msg_dynamixel_status_get_noutputs(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint8_t(msg,  40);
+    return _MAV_RETURN_uint8_t(msg,  72);
 }
 
 /**
- * @brief Get field angles from dynamixel_status message
+ * @brief Get field anglesMeasured from dynamixel_status message
  *
- * @return [rad] Current angles.
+ * @return [rad] Current angle measurements.
  */
-static inline uint16_t mavlink_msg_dynamixel_status_get_angles(const mavlink_message_t* msg, float *angles)
+static inline uint16_t mavlink_msg_dynamixel_status_get_anglesMeasured(const mavlink_message_t* msg, float *anglesMeasured)
 {
-    return _MAV_RETURN_float_array(msg, angles, 8,  8);
+    return _MAV_RETURN_float_array(msg, anglesMeasured, 8,  8);
+}
+
+/**
+ * @brief Get field anglesSet from dynamixel_status message
+ *
+ * @return [rad] Current angle targets.
+ */
+static inline uint16_t mavlink_msg_dynamixel_status_get_anglesSet(const mavlink_message_t* msg, float *anglesSet)
+{
+    return _MAV_RETURN_float_array(msg, anglesSet, 8,  40);
 }
 
 /**
@@ -245,7 +270,8 @@ static inline void mavlink_msg_dynamixel_status_decode(const mavlink_message_t* 
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     dynamixel_status->time_boot_us = mavlink_msg_dynamixel_status_get_time_boot_us(msg);
-    mavlink_msg_dynamixel_status_get_angles(msg, dynamixel_status->angles);
+    mavlink_msg_dynamixel_status_get_anglesMeasured(msg, dynamixel_status->anglesMeasured);
+    mavlink_msg_dynamixel_status_get_anglesSet(msg, dynamixel_status->anglesSet);
     dynamixel_status->noutputs = mavlink_msg_dynamixel_status_get_noutputs(msg);
 #else
         uint8_t len = msg->len < MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN? msg->len : MAVLINK_MSG_ID_DYNAMIXEL_STATUS_LEN;
--- omav/testsuite.h
+++ omav/testsuite.h
@@ -335,14 +335,15 @@ static void mavlink_test_dynamixel_status(uint8_t system_id, uint8_t component_i
         uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
         uint16_t i;
     mavlink_dynamixel_status_t packet_in = {
-        93372036854775807ULL,{ 73.0, 74.0, 75.0, 76.0, 77.0, 78.0, 79.0, 80.0 },125
+        93372036854775807ULL,{ 73.0, 74.0, 75.0, 76.0, 77.0, 78.0, 79.0, 80.0 },{ 297.0, 298.0, 299.0, 300.0, 301.0, 302.0, 303.0, 304.0 },221
     };
     mavlink_dynamixel_status_t packet1, packet2;
         memset(&packet1, 0, sizeof(packet1));
         packet1.time_boot_us = packet_in.time_boot_us;
         packet1.noutputs = packet_in.noutputs;
         
-        mav_array_memcpy(packet1.angles, packet_in.angles, sizeof(float)*8);
+        mav_array_memcpy(packet1.anglesMeasured, packet_in.anglesMeasured, sizeof(float)*8);
+        mav_array_memcpy(packet1.anglesSet, packet_in.anglesSet, sizeof(float)*8);
         
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
         if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
@@ -356,12 +357,12 @@ static void mavlink_test_dynamixel_status(uint8_t system_id, uint8_t component_i
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_dynamixel_status_pack(system_id, component_id, &msg , packet1.time_boot_us , packet1.noutputs , packet1.angles );
+    mavlink_msg_dynamixel_status_pack(system_id, component_id, &msg , packet1.time_boot_us , packet1.noutputs , packet1.anglesMeasured , packet1.anglesSet );
     mavlink_msg_dynamixel_status_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_dynamixel_status_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.time_boot_us , packet1.noutputs , packet1.angles );
+    mavlink_msg_dynamixel_status_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.time_boot_us , packet1.noutputs , packet1.anglesMeasured , packet1.anglesSet );
     mavlink_msg_dynamixel_status_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
@@ -374,7 +375,7 @@ static void mavlink_test_dynamixel_status(uint8_t system_id, uint8_t component_i
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
         
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_dynamixel_status_send(MAVLINK_COMM_1 , packet1.time_boot_us , packet1.noutputs , packet1.angles );
+    mavlink_msg_dynamixel_status_send(MAVLINK_COMM_1 , packet1.time_boot_us , packet1.noutputs , packet1.anglesMeasured , packet1.anglesSet );
     mavlink_msg_dynamixel_status_decode(last_msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
